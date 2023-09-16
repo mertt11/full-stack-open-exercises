b96acc15f9972b978b6d105248969b3361358326
Mert Gokce <mertdeneme10@outlook.com>
2023-09-16 14:22:59 +0300

    javascript 1.4

--- introduction-to-react/part1/src/App.jsx
+++ introduction-to-react/part1/src/App.jsx
@@ -35,23 +35,19 @@ const App = () => {
   
   const course = 'Half Stack application development'
 
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
   const parts=[
-    part1,
-    part2,
-    part3,
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
   ]
 
 
@@ -68,7 +64,7 @@ const App = () => {
       </p>
 
       <p>
-         <Total total={part1.exercises+part2.exercises+part3.exercises}/> 
+         <Total total={parts[0].exercises+parts[1].exercises+parts[2].exercises}/> 
       </p>
 
     </div>
