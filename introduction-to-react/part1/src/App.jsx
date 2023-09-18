79321bdbf70cbe1ff7515914ea02b77d3dc7c9ad
Mert Gokce <mertdeneme10@outlook.com>
2023-09-18 09:28:16 +0300

    some changes

--- introduction-to-react/part1/src/App.jsx
+++ introduction-to-react/part1/src/App.jsx
@@ -1,4 +1,4 @@
-const Header = (props) => {
+/* const Header = (props) => {
   return (
       <span>
         Name of the course is {props.course.name} 
@@ -74,4 +74,94 @@ const App = () => {
 
 export default App
 
-/* course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises */
+ */
+
+/* const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+export default App */
+
+
+/* import { useState } from 'react'
+
+const App = () => {
+
+  const [ counter, setCounter ] = useState(0)
+
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log('rendering...', counter)
+
+
+  return (
+    <div>{counter}</div>
+  )
+}
+
+export default App */
+
+
+import { useState } from 'react'
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+
+const App = () => {
+
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
+  return (
+    <div>
+      <Display counter={counter}/>
+      <button onClick={increaseByOne}>
+      {/* <button onClick={() => setCounter(counter + 1)}>  */} 
+      {/* <button onClick={() => console.log('clicked')}> */}  
+      {/* <button onClick={handleClick}> */}
+        plus
+      </button>
+
+      <button onClick={setToZero}>
+      zero
+      </button>
+
+    </div>
+  )
+}
+
+export default App
